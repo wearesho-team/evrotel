e03fd9b2b86ba9c531a3b0f60e19a3eefe2fd422
Alexander Letnikow <reclamme@gmail.com>
2018-10-08 13:41:43 +0300

    Add config interface

---
+++ src/ConfigInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Wearesho\Evrotel;
+
+/**
+ * Interface ConfigInterface
+ * @package Wearesho\Evrotel
+ */
+interface ConfigInterface
+{
+    public function getToken(): string;
+}
