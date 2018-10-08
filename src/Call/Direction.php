b1b645375855814560dc03a52a0e7ffe6f48144b
Alexander Letnikow <reclamme@gmail.com>
2018-10-08 14:36:43 +0300

    Add call fields constant interfaces

---
+++ src/Call/Direction.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Wearesho\Evrotel\Call;
+
+/**
+ * Interface Direction
+ * @package Wearesho\Evrotel\Call
+ */
+interface Direction
+{
+    public const INCOME = 'i';
+    public const OUTCOME = 'o';
+}
---
+++ src/Call/Status.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Wearesho\Evrotel\Call;
+
+/**
+ * Interface Status
+ * @package Wearesho\Evrotel\Call
+ */
+interface Status
+{
+    public const START = 'start';
+    public const END = 'end';
+}
