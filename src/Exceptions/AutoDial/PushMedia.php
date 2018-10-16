5da2fbd0223bcaf25f2e450c9995941fc0bd9119
Alexander Letnikow <reclamme@gmail.com>
2018-10-16 16:25:53 +0300

    Add PushMedia auto dial exception to be thrown while pushing media

---
+++ src/Exceptions/AutoDial/PushMedia.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Wearesho\Evrotel\Exceptions\AutoDial;
+
+/**
+ * Class PushMedia
+ * @package Wearesho\Evrotel\Exceptions\AutoDial
+ */
+class PushMedia extends \RuntimeException
+{
+}
