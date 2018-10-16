ca130849a733838b9f0a219ed945c8be51be4049
Alexander Letnikow <reclamme@gmail.com>
2018-10-16 15:35:27 +0300

    Extend ConfigTest to test bill code and base url

--- tests/ConfigTest.php
+++ tests/ConfigTest.php
@@ -12,6 +12,8 @@ use Wearesho\Evrotel;
 class ConfigTest extends TestCase
 {
     protected const TEST_TOKEN = 'testToken';
+    protected const TEST_BILL_CODE = 6667;
+    protected const TEST_BASE_URL = 'https://test.dev/';
 
     /** @var Evrotel\Config */
     protected $config;
@@ -19,12 +21,35 @@ class ConfigTest extends TestCase
     protected function setUp(): void
     {
         parent::setUp();
-        $this->config = new Evrotel\Config(static::TEST_TOKEN);
+        $this->config = new Evrotel\Config(
+            static::TEST_TOKEN,
+            static::TEST_BILL_CODE,
+            static::TEST_BASE_URL
+        );
     }
 
     public function testGetToken(): void
     {
         $token = $this->config->getToken();
         $this->assertEquals(static::TEST_TOKEN, $token);
+    }
+
+    public function testGetBillCode(): void
+    {
+        $billCode = $this->config->getBillCode();
+        $this->assertEquals(static::TEST_BILL_CODE, $billCode);
+    }
+
+    public function testGetBaseUrl(): void
+    {
+        $baseUrl = $this->config->getBaseUrl();
+        $this->assertEquals(static::TEST_BASE_URL, $baseUrl);
+    }
+
+    public function testDefaultBaseUrl(): void
+    {
+        $config = new Evrotel\Config(static::TEST_TOKEN, static::TEST_BILL_CODE);
+        $baseUrl = $config->getBaseUrl();
+        $this->assertEquals(Evrotel\ConfigInterface::DEFAULT_BASE_URL, $baseUrl);
     }
 }
