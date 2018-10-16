b9fb04a7326c18a399e7d7617c0be8cb36c43ae7
Alexander Letnikow <reclamme@gmail.com>
2018-10-16 15:51:00 +0300

    Add base autodial RequestInterface implementation in RequestTrait

---
+++ src/AutoDial/RequestTrait.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Wearesho\Evrotel\AutoDial;
+
+/**
+ * Trait RequestTrait
+ * @package Wearesho\Evrotel\AutoDial
+ */
+trait RequestTrait
+{
+    /** @var string */
+    protected $fileName;
+
+    /** @var string */
+    protected $phone;
+
+    public function getPhone(): string
+    {
+        return $this->phone;
+    }
+
+    public function getFileName(): string
+    {
+        return $this->fileName;
+    }
+
+    protected function validatePhone(string $phone): void
+    {
+        if (!preg_match('/^380\d{9}$/', $phone)) {
+            throw new \InvalidArgumentException("Invalid ukranian phone format: {$phone}");
+        }
+    }
+
+    protected function validateFileName(string $fileName): void
+    {
+        if (!preg_match('/^.+\.wav$/', $fileName)) {
+            throw new \InvalidArgumentException('File name have to contain .wav extension');
+        }
+    }
+}
