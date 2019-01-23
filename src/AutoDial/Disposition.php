620afc747f0b68240bed0791daa289391a949711
Alexander Letnikow <reclamme@gmail.com>
2019-01-23 17:13:28 +0200

    Add disposition interface for auto dial worker result constants

---
+++ src/AutoDial/Disposition.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Wearesho\Evrotel\AutoDial;
+
+/**
+ * Interface Disposition
+ * @package Wearesho\Evrotel\AutoDial
+ */
+interface Disposition
+{
+    public const ANSWER = 'ANSWER';
+    public const BAD = 'BAD';
+    public const BUSY = 'BUSY';
+    public const NO_ANSWER = 'NOANSWER';
+    public const CONGESTION = 'CONGESTION';
+    public const NONE = 'none';
+}
